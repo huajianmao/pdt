2c2cc5287626684cc27d6af3ab878e095a40277c
Huajian Mao <huajianmao@gmail.com>
2016-03-27 21:20:13 +0800

    Replace all wdt strings to pdt.

--- scripts/pack-to-release.sh
+++ scripts/pack-to-release.sh
@@ -32,11 +32,44 @@ cp scripts/setup.sh $RELEASE_DIR/scripts/setup.sh
 
 cd $RELEASE_DIR/src
 git clone https://github.com/google/double-conversion
+rm -rf double-conversion/.git
 git clone https://github.com/gflags/gflags
+rm -rf gflags/.git
 git clone https://github.com/google/glog
+rm -rf glog/.git
 git clone https://github.com/google/googletest
+rm -rf googletest/.git
 git clone https://github.com/facebook/folly
+rm -rf folly/.git
 git clone https://github.com/facebook/wdt
+rm -rf wdt/.git
+
+## Replace all wdt to pdt
+DIR=wdt
+
+sed -i 's/wdt/pdt/g; s/wdT/pdT/g; s/wDt/pDt/g; s/wDT/pDT/g; s/Wdt/Pdt/g; s/WdT/PdT/g; s/WDt/PDt/g; s/WDT/PDT/g' `grep wdt -irl $DIR`
+
+pushd .
+cd $DIR
+declare -A nameConv 
+nameConv[wdt]="pdt"
+nameConv[wdT]="pdT"
+nameConv[wDt]="pDt"
+nameConv[wDT]="pDT"
+nameConv[Wdt]="Pdt"
+nameConv[WdT]="PdT"
+nameConv[WDt]="PDt"
+nameConv[WDT]="PDT"
+keywords="wdt wdT wDt wDT Wdt WdT WDt WDT"
+for keyword in $keywords; do
+  files=`find . -name "*${keyword}*"`
+  for file in $files; do
+    newpath=`echo $file | sed "s/$keyword/${nameConv[$keyword]}/g"`
+    mv $file $newpath 
+  done
+done
+popd
+## done of replacing
 
 cp -a $RELEASE_DIR $BUILD_DIR/
 cd $BUILD_DIR/$VERSION
